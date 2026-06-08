bfd602e4285e583bd6fe1283a2b4c1ccb4cf406f
Koushik <molugurikoushik68@gmail.com>
2026-06-08 11:45:51 +0530

    Update summarizer.py
    Small is not working, let's see with tiny

--- src/summarizer.py
+++ src/summarizer.py
@@ -50,7 +50,7 @@ class QualityScorer:
 class AdvancedPromptSummarizer:
     """Advanced dialogue summarizer with multiple strategies"""
     
-    def __init__(self, model_name="google/flan-t5-small"):
+    def __init__(self, model_name="google/flan-t5-tiny"):
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
